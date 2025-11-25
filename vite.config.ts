3e722ca29da2cee6bd59b07f1afb6cbec7e9d4ef
Alana Kovalski de Angeli <161781119+alanakov@users.noreply.github.com>
2025-11-25 19:04:41 +0000

    feat: update deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@
 
   export default defineConfig({
     plugins: [react()],
-    base: "/Agrodetech-pitch",
+    base: "/Agrodetech-pitch/",
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
